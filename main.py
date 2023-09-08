d173a6bbbdf45da742fb14bae04fdf593fa028cd
Talha Bakır <motalhabakir@gmail.com>
2023-09-08 14:02:31 +0300

    a working version of the game coded

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ y_min, y_max = -200, 180
 
 def belir_ve_kaybol():
     while True:
-        for i in range(20):
+        for i in range(30):
             x = random.randint(x_min, x_max)
             y = random.randint(y_min, y_max)
             freaking_turtle.goto(x, y)
             freaking_turtle.showturtle()
-            time.sleep(0.5)
+            time.sleep(1)
             freaking_turtle.hideturtle()
         freaking_turtle.clear()
         turtle.done()
@@ -43,7 +43,7 @@ timeR.goto(0, ekran_yukseklik // 2.8 - 20)
 timeR.hideturtle()
 timeR.clear()
 def geri_sayim():
-    for i in range(10, 0, -1):
+    for i in range(30, 0, -1):
         timeR.clear()
         timeR.write("Time: {}".format(i), align="center", font=("Arial", 24, "normal"))
         time.sleep(1)
@@ -52,15 +52,36 @@ def geri_sayim():
     timeR.write("Game Over", align="center", font=("Arial", 24, "normal"))
     turtle.done()
 
+
+scoreR = turtle.Turtle()
+scoreR.speed(0)
+scoreR.color("black")
+scoreR.penup()
+scoreR.hideturtle()
+scoreR.goto(0, ekran_yukseklik // 2.9 + 40)
+scoreR.write("Score: 0", align="center", font=("Arial", 24, "normal"))
+
+skor = 0
+
+def scoreCount(x, y):
+    global skor
+    if x_min <= x <= x_max and y_min <= y <= y_max:
+        if abs(x - freaking_turtle.xcor()) < 10 and abs(y - freaking_turtle.ycor()) < 10:
+            skor += 1
+            scoreR.clear()
+            scoreR.write("Skor: {}".format(skor), align="center", font=("Arial", 24, "normal"))
+
+
 if __name__ == '__main__':
     threading.Thread(target=geri_sayim).start()
     threading.Thread(target=belir_ve_kaybol).start()
 
+    screen.onclick(scoreCount)
 
 
 
 
-screen.exitonclick()
+turtle.mainloop()
 
 
 
